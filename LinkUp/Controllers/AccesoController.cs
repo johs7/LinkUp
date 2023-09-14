800b8970ec982e7b7662a3018141db3eb2ba7a78
Johanssen <roquejohanssen@gmai.com>
2023-09-14 09:51:33 -0600

    login usuarios

--- LinkUp/Controllers/AccesoController.cs
+++ LinkUp/Controllers/AccesoController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using LinkupCN.CN;
 using System.Web;
 using System.Web.Mvc;
+using LinkupEDM.AppModel;
 
 namespace LinkUp.Controllers
 {
@@ -12,6 +14,26 @@ namespace LinkUp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string correo,string clave)
+        {
+            Clientes oClientes= new Clientes();
+            oClientes = new ClientesCN().Listar().Where(u => u.Correo == correo &&
+            u.Clave == RecursosCN.ConvertirSha256(clave)).FirstOrDefault();
+            if(oClientes == null)
+            {
+                ViewBag.Error = "Correo o contraseña invalida";
+                return View();
+            }
+            else
+            {
+                ViewBag.Error = null;
+                return RedirectToAction("Clientes", "Home");
+            }
+          
         }
     }
 }
