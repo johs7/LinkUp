e89838f55c99723a60a6250a882560c2a296c331
Johanssen <roquejohanssen@gmai.com>
2023-10-05 08:49:55 -0600

    agregando archivos iniciales tipo de cambio

---
+++ LinkUpAdmin/Controllers/TipoCambioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkUpAdmin.Controllers
+{
+    public class TipoCambioController : Controller
+    {
+        // GET: TipoCambio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
